0729eca74c328e2ae7d7313e55be8b3d3ebad433
Zehadh <zehadh45@gmail.com>
2021-02-14 21:38:36 +0600

    final push

--- app.js
+++ app.js
@@ -69,49 +69,37 @@ const createSlider = () => {
     return;
   }
   // crate slider previous next area
-  sliderContainer.innerHTML = '';
-  const prevNext = document.createElement('div');
-  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
-  prevNext.innerHTML = `
-  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
-  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
-  `;
-
-  sliderContainer.appendChild(prevNext);
-  document.querySelector('.main').style.display = 'block';
-  // hide image aria
-  imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
-  if(duration<0){
-    sliders.forEach(slide => {
-      let item = document.createElement('div')
-      item.className = "slider-item";
-      item.innerHTML = `<img class="w-100"
-      src="${slide}"
-      alt="">`;
-      sliderContainer.appendChild(item);
-    })
-    changeSlide(0)
-    timer = setInterval(function () {
-      slideIndex++;
-      changeSlide(slideIndex);
-    }, duration * (-1));
-  }
-  else{
-    sliders.forEach(slide => {
-      let item = document.createElement('div')
-      item.className = "slider-item";
-      item.innerHTML = `<img class="w-100"
-      src="${slide}"
-      alt="">`;
-      sliderContainer.appendChild(item);
-    })
-    changeSlide(0)
-    timer = setInterval(function () {
-      slideIndex++;
-      changeSlide(slideIndex);
-    }, duration);
-  }
+ if(duration<0){
+    alert("Please try again with positive value");
+ }
+ else{
+    sliderContainer.innerHTML = '';
+    const prevNext = document.createElement('div');
+    prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+    prevNext.innerHTML = `
+    <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+    <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+    `;
+  
+    sliderContainer.appendChild(prevNext);
+    document.querySelector('.main').style.display = 'block';
+    // hide image aria
+    imagesArea.style.display = 'none';
+      sliders.forEach(slide => {
+        let item = document.createElement('div')
+        item.className = "slider-item";
+        item.innerHTML = `<img class="w-100"
+        src="${slide}"
+        alt="">`;
+        sliderContainer.appendChild(item);
+      })
+      changeSlide(0)
+      timer = setInterval(function () {
+        slideIndex++;
+        changeSlide(slideIndex);
+      }, duration);
+ }
 }
 
 // change slider index 
